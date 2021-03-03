7c7ccf3e131171630b87d43347ea174b9c90cff8
zhaoxy <zhaoxydyx@163.com>
2021-03-03 22:29:00 +0800

    测试

--- 01.Start-Spring-Boot/src/main/java/com/springboot/demo/DemoApplication.java
+++ 01.Start-Spring-Boot/src/main/java/com/springboot/demo/DemoApplication.java
@@ -16,5 +16,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("http://localhost:8080");
 	}
 }
